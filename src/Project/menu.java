1d47b8dc509d18c7ddc321f7d21f450ed13ca7c2
Juzzke93 <Joakimosterberg@hotmail.com>
2020-05-12 11:09:01 +0200

    'merge'

--- src/Project/menu.java
+++ src/Project/menu.java
@@ -2,16 +2,17 @@ package Project;
 
 import java.util.Scanner;
 
+
 public class menu {
     public static void main(String[] args) {
-        Member nyMedlem = new Member(1, 19950409, "Aljosa", "Vuckovic", "Student", new int[]{0, 0, 0}, false, 0);
-        Book nyBok = new Book("Game Of Thrones", "George R.R. Martin", )
+
 
         Scanner scan = new Scanner(System.in);
         int choice;
 
         do {
-            System.out.println("Librarysystem \n=============");
+            System.out.println("Welcome to the library! \n=============");
+            System.out.println("In the menu below you will see the current options that the library has.");
             System.out.println("1. Create user");
             System.out.println("2. Rent book");
             System.out.println("3. Return book");
@@ -21,25 +22,35 @@ public class menu {
 
             if (choice == 1) {
                 int personalnumber = 0;
-                String firstName;
-                String lastName;
-
-                System.out.println("Personal number: ");
+                System.out.print("Personal number: ");
                 personalnumber = scan.nextInt();
 
-                System.out.println("First name: ");
-                firstName = scan.nextLine();
+                scan.nextLine();
+                System.out.print("First name: ");
+                String firstName = scan.nextLine();
 
-                System.out.println("Last name: ");
-                lastName = scan.nextLine();
+                System.out.print("Last name: ");
+                String lastName = scan.nextLine();
 
-                Member newMember = new Member();
+
+
+
+
+                System.out.println("\nA new user has been created. The information for the user is, \n"
+                        + "personalnumber: " + personalnumber
+                        + " \nfirstname: " + firstName
+                        + " \nlastname: " + lastName + "\n");
+
             }
             if (choice == 2) {
                 System.out.println("Which book are you searching for? ");
+                String bookTitle;
+                bookTitle = scan.nextLine();
+
             }
             if (choice == 3) {
-                System.out.println("Enter your ");
+                System.out.println("Enter your userid");
+
             }
         }
         while (choice != 5);
