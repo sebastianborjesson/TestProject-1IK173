0745c63295dd36af44627e5a7db77c7cab3398f8
unknown <aljosavp@gmail.com>
2020-05-20 13:53:28 +0200

    ok

---
+++ Test/Project/LibrarianTest.java
@@ -0,0 +1,61 @@
+package Project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class LibrarianTest {
+
+    private LibraryStore ls = mock(LibraryStore.class);
+    private Librarian cut = new Librarian(ls);
+
+
+    @Test
+    void createAccount() {
+       /* cut.createAccount(972379, " Bullen","korv","Student");
+
+        when(ls.getAllMembers()).thenReturn(new Member[]{
+                new Member(1234,972379, "Bullen","Korv","Student", 0,0,false,0,null)
+        });
+        assertEquals(cut.).addMember(1234,972379,"Bullen", "Korv", "Student");
+
+        assertEquals();
+
+        */
+
+
+    }
+
+    @Test
+    void checkBanned() {
+    }
+
+    @Test
+    void banMember() {
+    }
+
+    @Test
+    void suspendMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void borrowBook() {
+
+        when(ls.getAllMembers())
+                .then(new Member{
+                new Member(1234,972379, "Bullen","Korv","Student", 0,0,false,0,null)
+        };
+
+        when(ls.getAllBooks()).
+
+    }
+
+    @Test
+    void returnBook() {
+    }
+}
